369624cd5a4e71032356401b8f0ec2dddd0e45b8
Richard Smith <richard.smith@worldvision.com.au>
2012-05-31 15:18:01 +1000

    tests

--- TaskSystem/TaskSystem/Data/Configuration/UserTaskEntityConfiguration.cs
+++ TaskSystem/TaskSystem/Data/Configuration/UserTaskEntityConfiguration.cs
@@ -14,17 +14,7 @@ namespace TaskSystem.Data.Configuration
             //HasRequired(a => a.TaskType)
             //   .WithMany().HasForeignKey(x => x.TaskType);
 
-             HasRequired(x => x.TaskType);//..Map(x => x.MapKey("TransactionID"));
-
-            //Property(x => x.TaskType);
-
-            //Property(x => x.StatusId).HasColumnName("Status").IsRequired();
-            //Property(x => x.EchoServiceCallId).HasColumnName("EchoServiceCall").IsRequired();
-            //Property(x => x.DateModified).IsOptional();
-            //Property(x => x.DateAdded).IsRequired();
-            //Property(x => x.FailureMessage).IsOptional().IsMaxLength();
-            //Property(x => x.Attempts).IsOptional();
-            //Property(x => x.TransactionId).IsRequired();
+            HasRequired(x => x.UserTaskType);
 
 
         }
--- TaskSystem/TaskSystem/Models/UserTaskType.cs
+++ TaskSystem/TaskSystem/Models/UserTaskType.cs
@@ -1,6 +1,6 @@
 namespace TaskSystem.Models
 {
-    public class TaskType
+    public class UserTaskType
     {
         public int Id { get; set; }
         public string Name { get; set; }
